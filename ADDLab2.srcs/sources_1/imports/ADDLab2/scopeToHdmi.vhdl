fb45dc3c6a03baf0e826efc033ec66c555252780
eharb04 <eharb004@gmail.com>
2026-09-21 14:36:57 -0600

    process(clk)

--- ADDLab2.srcs/sources_1/imports/ADDLab2/scopeToHdmi.vhdl
+++ ADDLab2.srcs/sources_1/imports/ADDLab2/scopeToHdmi.vhdl
@@ -9,7 +9,7 @@ use work.scopeToHdmi_package.all;
 entity scopeToHdmi is
     PORT ( sysClk : in  STD_LOGIC;
          resetn : in  STD_LOGIC;
-         btn: in	STD_LOGIC_VECTOR(2 downto 0);
+         btn: in STD_LOGIC_VECTOR(2 downto 0);
          tmdsDataP : out  STD_LOGIC_VECTOR (2 downto 0);
          tmdsDataN : out  STD_LOGIC_VECTOR (2 downto 0);
          tmdsClkP : out STD_LOGIC;
@@ -31,6 +31,8 @@ architecture structure of scopeToHdmi is
     signal videoClk, videoClk5x, clkLocked: STD_LOGIC;
     
     signal hs, vs, de: STD_LOGIC; --Intermediate
+    signal reset: STD_LOGIC;
+    signal prevButton, currButton, activeButton: STD_LOGIC_VECTOR(2 downto 0);
 
 begin
 
@@ -48,7 +50,7 @@ begin
     sf: scopeFace
         PORT MAP (clk => videoClk,
                   resetn => resetn,
-                  pixelH => pixelHorz,
+                  pixelHorz => pixelHorz,
                   pixelVert => pixelVert,
                   triggerVolt => triggerVolt,
                   triggerTime => triggerTime,
@@ -56,14 +58,16 @@ begin
                   green => green,
                   blue => blue,
                   ch1 => ch1Wave,
-                  ch2 => ch1Wave);
+                  ch1Enb => '1',
+                  ch2 => ch1Wave,
+                  ch2Enb => '1');
                  
 
     hdmi_inst: hdmi_tx_0
         PORT MAP (
             pix_clk => videoClk,
             pix_clkx5 => videoClk5x,
-            rst => not resetn,
+            rst => reset,
             hsync => hs,
             vsync => vs,
             vde => de,
@@ -96,11 +100,39 @@ begin
     -- increment/decrement the triggerTime or triggerVolt values
     ------------------------------------------------------------------------------
  
-    process(clk)
+    process(sysclk)
     begin
-        
-    end process
+        if rising_edge (sysclk) then
+            currButton <= btn; --Button values taken on clk edge
+            activeButton <= currButton xor prevButton; --To see change
+            if resetn = '0' then
+                activeButton <= "000";
+                prevButton <= "111"; --Assume prev was not pressed
+                currButton <= "111"; --Active Low, so set to not being pressed
+                triggerTime <= STD_LOGIC_VECTOR(TO_UNSIGNED(500, VIDEO_WIDTH_IN_BITS)); --Somewhere around the middle
+                triggerVolt <= STD_LOGIC_VECTOR(TO_UNSIGNED(300, VIDEO_WIDTH_IN_BITS)); 
+            elsif activeButton > 0 then --Something has changed state
+                if (activeButton(0) = '1' and currButton(0) = '1') then --PL_KEY4 changed to being released
+                    if (currButton(2) = '1') then --PL_KEY2 is not being pressed
+                        triggerTime <= triggerTime - 10;
+                    elsif (currButton(2) = '0') then --PL_KEY2 is being pressed
+                        triggerTime <= triggerTime + 10;
+                    end if;
+                end if;
+                
+                if (activeButton(1) = '1' and currButton(1) = '1') then --PL_KEY3 changed to being released
+                    if (currButton(2) = '1') then --PL_KEY2 is not being pressed
+                        triggerVolt <= triggerVolt - 10;
+                    elsif (currButton(2) = '0') then --PL_KEY2 is being pressed
+                        triggerVolt <= triggerVolt + 10;
+                    end if;
+                end if;
+            end if;
+            prevButton <= currButton;
+        end if;
+    end process;
  
+    reset <= not resetn;
     ch1Wave <= '1' when  (pixelHorz = pixelVert) else '0';
     ch2Wave <= '1' when  (pixelVert = triggerVolt) else '0';
 
