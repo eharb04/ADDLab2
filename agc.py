aba5432d792400fdca75a5c5d9fb7773a3459666
eormes <ethanormes@gmail.com>
2026-09-23 11:14:11 -0600

    updated grid acg

--- agc.py
+++ agc.py
@@ -26,8 +26,8 @@ def gridH(f):
   print("gridH <= '1' when (((pixelHorz > L_EDGE + BORDER_LINE_WIDTH) and", file=f)
   print(f"\t\t\t\tpixelHorz < R_EDGE - BORDER_LINE_WIDTH)) and(", file=f)
   for i in range (1,9):
-    print(f"\t\t\t\tpixelVert = T_EDGE + BORDER_LINE_WIDTH + {100*i} or",file = f)
-  print(f"\t\t\t\tpixelVert = T_EDGE + BORDER_LINE_WIDTH + {100*(i+1)}) else '0';",file = f)
+    print(f"\t\t\t\tpixelVert = T_EDGE + BORDER_LINE_WIDTH + {600/10*i} or",file = f)
+  print(f"\t\t\t\tpixelVert = T_EDGE + BORDER_LINE_WIDTH + {600/10*(i+1)}) else '0';",file = f)
 
 def gridV(f):
   print("gridV <= '1' when (((pixelVert > T_EDGE + BORDER_LINE_WIDTH) and", file=f)
